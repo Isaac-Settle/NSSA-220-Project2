c1443e26fffc023a45e694cdb7ccac889880fb1f
Isaac-Settle <isettle82@gmail.com>
2022-11-03 15:14:34 -0400

    starting to implement

--- filter_packets.py
+++ filter_packets.py
@@ -1,3 +1,10 @@
-def filter() :
-	print('called filter function in filter_packets.py')
-	
+import re
+def filter():
+	filename = r"Captures\Node1.txt"
+	with open(filename) as file:
+		for line in file:
+			if re.findall("Echo (ping) Request", line):
+				print(line)
+			# do things
+
+filter()
