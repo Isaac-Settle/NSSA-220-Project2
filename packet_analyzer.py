22e7fb59dc45ca009a9ee3f05beeabf72c4e6806
Jose Nunez <53985897+jan5326@users.noreply.github.com>
2022-11-07 22:36:41 -0500

    Update packet_analyzer.py

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -1,7 +1,11 @@
+# NSSA 220 - Project 2 - Packet Capture Analysis Tool
+# Riley Basile-Benson, Isaac Settle, and Jose Nunez
+
+
 from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
 filter()
 parse()
-compute()
+compute(node1_list, node2_list, node3_list, node4_list)
