9ce9ccb70effde30ef32df0a22da5b2f66997a6c
Jose Nunez <53985897+jan5326@users.noreply.github.com>
2022-11-07 22:33:13 -0500

    Update compute_metrics.py

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,3 +1,16 @@
-def compute() :
-	print('called compute function in compute_metrics.py')
-
+def compute(node1_list, node2_list, node3_list, node4_list):
+	#Compute method take in 4 lists, each corresponding to a node. This function will compute metrics for each of the node files using the list passed in.
+	
+	#Variables
+    	node1_ip = "192.168.100.1" 
+    	node2_ip = "192.168.100.2"
+    	node3_ip = "192.168.200.1"
+    	node4_ip = "192.168.200.2"
+	
+	# Do computations for each node
+	# Node 1
+	# Node 2
+	# Node 3
+	# Node 4
+	
+    	#Output to a CSV file
