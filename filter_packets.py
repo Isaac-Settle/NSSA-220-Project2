dc95dd4a29d31088f3df2bfc98accedd3a637fec
isaac-settle <isettle82@gmail.com>
2022-11-10 14:46:57 -0500

    filter_packets currently working. Still need to implement main

--- filter_packets.py
+++ filter_packets.py
@@ -1,10 +1,23 @@
 import re
 def filter():
-	filename = r"Captures\Node1.txt"
+	filename = "Captures/example.txt"
 	with open(filename) as file:
+		prevLine = ""
 		for line in file:
-			if re.findall("Echo (ping) Request", line):
-				print(line)
+			if re.search('Echo', line) != None:
+				print(prevLine.strip("\n"))
+				print(line.strip("\n"))
+				var = True
+				increment = 0
+				while(var and increment<1):
+					line = file.readline()
+					if(re.search('No.', line) == None):
+						print(line.strip("\n"))
+					else:
+						var = False
+					if(line==""):
+						increment+=1
+			prevLine = line
 			# do things
 
 filter()
