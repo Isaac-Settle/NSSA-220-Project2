63b961ba9b8b6401ffd3a13d5e29bf3247eac119
GCCIS <no@no.com>
2022-11-29 15:00:49 -0500

    Finished, just needs comments and cleaned up

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,5 +1,5 @@
 from Packet import Packet
-
+import sys
 
 def calc_num_echo_req_replies_sent(node_list, source_node_ip):
     requests = []
@@ -65,63 +65,60 @@ def calc_reply_delay(requests_recvd, replies_sent):
     return reply_delay/num_replies
 
 
-def calc_average_hop_count(requests_recvd):
+def calc_average_hop_count(replies_recvd):
     STANDARD_TTL = 128
     total_ttl = 0
     num_ttl = 0
-    for request in requests_recvd:
-        total_ttl += (STANDARD_TTL - request.ttl) + 1
+    for reply in replies_recvd:
+        total_ttl += (STANDARD_TTL - reply.ttl) + 1
         num_ttl += 1
-    return total_ttl/num_ttl
+    return total_ttl/(num_ttl)
 
 
 def compute(node1_list, node2_list, node3_list, node4_list):
     # Compute method take in 4 lists, each corresponding to a node. This function will compute metrics for each of the node files using the list passed in.
 
     # Variables
-    NODE1_IP = "192.168.100.1"
-    NODE2_IP = "192.168.100.2"
-    NODE3_IP = "192.168.200.1"
-    NODE4_IP = "192.168.200.2"
+    NodeIPs = {}
+    NodeIPs[1] = "192.168.100.1"
+    NodeIPs[2] = "192.168.100.2"
+    NodeIPs[3] = "192.168.200.1"
+    NodeIPs[4] = "192.168.200.2"
+
+    lists = [
+        node1_list, node2_list, node3_list, node4_list
+    ]
 
     # Do computations for each node
-    # Node 1
 
-    n1_requests_s, n1_replies_s = calc_num_echo_req_replies_sent(
-        node1_list, NODE1_IP)
-    n1_requests_r, n1_replies_r = calc_num_echo_req_replies_recvd(
-        node1_list, NODE1_IP)
+    f = open('output.csv', 'w')
+    sys.stdout = f
 
-    print("Echo Requests Sent,Echo Requests Received,Echo Replies Sent,Echo Replies Received")
-    print(len(n1_requests_s), len(n1_requests_r),
-          len(n1_replies_s), len(n1_replies_r), sep=",")
-    request_bytes_sent, request_data_sent, request_bytes_recvd, request_data_recvd = calc_request_bytes(
-        n1_requests_s, n1_requests_r)
 
-    print("Echo Request Bytes Sent (bytes),Echo Request Data Sent (bytes)")
-    print(request_bytes_sent, request_data_sent, sep=',')
-    print("Echo Request Bytes Received (bytes),Echo Request Data Received (bytes)")
-    print(request_bytes_recvd, request_data_recvd, sep=',', end="\n\n")
-    rtt = calc_rtt(n1_requests_s,n1_replies_r)
-    print("Average RTT (milliseconds),{:.2f}".format(rtt))
-    print("Echo Request Throughput (kB/sec),{:.1f}".format(request_bytes_sent/(rtt*len(n1_requests_s))))
-    print("Echo Request Goodput (kB/sec),{:.1f}".format(request_data_sent/(rtt*len(n1_requests_s))))
-    print("Average Reply Delay (microseconds),{:.2f}".format(calc_reply_delay(n1_requests_r, n1_replies_s)*(10**6)))
-    print("Average Echo Request Hop Count,{:.2f}".format(calc_average_hop_count(n1_requests_r)))
+    for i in range(4):
 
-    # Node 2
+        NODE_IP = NodeIPs[i+1]
 
-    # n2_requests, n2_replies = calc_num_echo_req_replies_sent(
-    #     node2_list, node2_ip)
-    # n3_requests, n3_replies = calc_num_echo_req_replies_sent(
-    #     node3_list, node3_ip)
-    # n4_requests, n4_replies = calc_num_echo_req_replies_sent(
-    #     node4_list, node4_ip)
-    # print(n2_requests, n2_replies, sep="\t")
-    # print(n3_requests, n3_replies, sep="\t")
-    # print(n4_requests, n4_replies, sep="\t")
+        requests_sent, replies_sent = calc_num_echo_req_replies_sent(
+            lists[i], NODE_IP)
+        requests_recvd, replies_recvd = calc_num_echo_req_replies_recvd(
+            lists[i], NODE_IP)
 
-    # Node 3
-    # Node 4
 
-    # Output to a CSV file
+        print('Node ', i+1, end="\n\n")
+        print("Echo Requests Sent,Echo Requests Received,Echo Replies Sent,Echo Replies Received")
+        print(len(requests_sent), len(requests_recvd),
+            len(replies_sent), len(replies_recvd), sep=",")
+        request_bytes_sent, request_data_sent, request_bytes_recvd, request_data_recvd = calc_request_bytes(
+            requests_sent, requests_recvd)
+
+        print("Echo Request Bytes Sent (bytes),Echo Request Data Sent (bytes)")
+        print(request_bytes_sent, request_data_sent, sep=',')
+        print("Echo Request Bytes Received (bytes),Echo Request Data Received (bytes)")
+        print(request_bytes_recvd, request_data_recvd, sep=',', end="\n\n")
+        rtt = calc_rtt(requests_sent,replies_recvd)
+        print("Average RTT (milliseconds),{:.2f}".format(rtt))
+        print("Echo Request Throughput (kB/sec),{:.1f}".format(request_bytes_sent/(rtt*len(requests_sent))))
+        print("Echo Request Goodput (kB/sec),{:.1f}".format(request_data_sent/(rtt*len(requests_sent))))
+        print("Average Reply Delay (microseconds),{:.2f}".format(calc_reply_delay(requests_recvd, replies_sent)*(10**6)))
+        print("Average Echo Request Hop Count,{:.2f}".format(calc_average_hop_count(replies_recvd)), end="\n\n")
